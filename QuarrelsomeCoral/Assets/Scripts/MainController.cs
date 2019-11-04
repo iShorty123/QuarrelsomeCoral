779a1788c28851fc6555a2ddca3890842bf5acf4
Adriana Padilla <adriana.m.padilla@hotmail.com>
2019-11-04 13:39:05 -0800

    Submarine Random Start

--- QuarrelsomeCoral/Assets/Scripts/MainController.cs
+++ QuarrelsomeCoral/Assets/Scripts/MainController.cs
@@ -45,12 +45,43 @@ public class MainController : MonoBehaviour
     }
 
     Vector3 FindEmpty(){
-        //foreach (var pos in Map.cellBounds.allPositionsWithin)
-        //{
 
-        //    if(
+        Vector3 position = new Vector3(0, 0, -4);
+        print(Map.cellBounds.yMin);
+        foreach (Vector3Int pos in Map.cellBounds.allPositionsWithin) {
 
-        //}
-        return transform.position;
+            if (pos.y < Map.cellBounds.yMin + 6 || pos.y > Map.cellBounds.yMax - 6 || pos.x < Map.cellBounds.xMin + 8 || pos.x > Map.cellBounds.xMax - 8) continue;
+
+            if (Map.GetTile(pos) == null)
+            {
+                if (CheckSurroundings(pos))
+                {
+                    position = pos;
+                    position.z = -4;
+
+                    MainCamera.transform.position = new Vector3(pos.x, pos.y, -10);
+
+                    //break;
+                }
+            }
+        }
+           
+            
+        return position;
+    }
+
+    bool CheckSurroundings(Vector3Int pos){
+
+        for (int i = pos.x - 7; i < pos.x + 7; i++) {
+            for (int j = pos.y - 5; j < pos.y + 5; j++)
+            {
+                Vector3Int p = new Vector3Int(i, j, pos.z);
+                if (Map.GetTile(p)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
