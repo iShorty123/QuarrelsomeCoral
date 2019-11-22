40f98f9fffd0d46e10d25fa10b102320bcf96fcd
Adriana Padilla <adriana.m.padilla@hotmail.com>
2019-11-22 14:22:59 -0800

    Initial MainMenu, Tutorial and Settings scenes
    Open MainMenu scene to start at menu

---
+++ QuarrelsomeCoral/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void GotoPlayScene()
+    {
+        SceneManager.LoadScene("World_ZB338");
+    }
+
+    public void GotoTutorialScene()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void GotSettingsScene()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void GotoQuitScene()
+    {
+        Application.Quit();
+    }
+
+    public void GotoPauseScene()
+    {
+        SceneManager.LoadScene("Pause");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
